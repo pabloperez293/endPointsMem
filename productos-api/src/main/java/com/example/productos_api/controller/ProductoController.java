9d4b11d6e7c40fec90ac97eacc40086c4416ddba
Pablo Perez <paperezpablo@gmail.com>
2026-07-04 15:56:16 -0300

    Metodos

--- productos-api/src/main/java/com/example/productos_api/controller/ProductoController.java
+++ productos-api/src/main/java/com/example/productos_api/controller/ProductoController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.productos_api.Model.Producto;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -42,6 +45,36 @@ public class ProductoController {
         //TODO: process POST request
         productos.add(producto);
         return producto;
+    }
+
+    @PutMapping("/{id}")
+    public Producto actualizarProducto(@PathVariable Long id, @RequestBody Producto productoActualizado) {
+        //TODO: process PUT request
+        for (Producto producto : productos) {
+
+            if (producto.getId().equals(id)) {
+
+                producto.setNombre(productoActualizado.getNombre());
+                producto.setPrecio(productoActualizado.getPrecio());
+                return producto;
+            }
+        }
+        return null; // Retorna null si no se encuentra el producto con el ID especificado
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminarProducto(@PathVariable Long id) {
+        // agregar delete
+        for (Producto producto : productos) {
+
+            if(producto.getId().equals(id)) {
+                productos.remove(producto);
+
+                return "productos eliminados";
+            }
+
+        }
+        return "Producto no encontrado";
     }
        
 }
