2cd5f28fbd1a5fa6182327c7a8a7bf18b4c5840c
Pablo Perez <paperezpablo@gmail.com>
2026-07-04 16:13:41 -0300

    cambiosCruds

--- productos-api/src/main/java/com/example/productos_api/controller/ProductoController.java
+++ productos-api/src/main/java/com/example/productos_api/controller/ProductoController.java
@@ -11,7 +11,6 @@ import com.example.productos_api.Model.Producto;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,13 +19,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RestController
 @RequestMapping("/productos")
 public class ProductoController {
+    
     private List<Producto> productos = new ArrayList<>();
 
-    @GetMapping("path")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
-    }
-    
+    @GetMapping
     public List<Producto> obtenerProductos() {
         return productos;
     }
